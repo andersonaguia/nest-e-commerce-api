77bc49820ce9a24878890f012f5bab1800149193
Anderson Aguiar <andersonlaguiar@gmail.com>
2022-11-27 00:02:06 -0300

    swagger config

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+  .setTitle('E-commerce API')
+  .setDescription('API para gerenciamento de e-commerce')
+  .setVersion('1.0')
+  .addTag('E-commerce')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -2,13 +2,19 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpS
 import { ProductsService } from '../service/products.service';
 import { CreateProductDto } from '../dto/create-product.dto';
 import { ProductEntity } from '../entities/product.entity';
+import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-
+@ApiTags('products')
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @ApiOperation({summary: "Create Product"})
+  @ApiResponse({
+    status: 201,
+    description: 'Produto cadastrado com sucesso.'
+  }) 
   async create(@Body() createProductDto: CreateProductDto): Promise<ProductEntity> {
     try{
       return await this.productsService.create(createProductDto);
